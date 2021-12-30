9de15602e97a299309d31acdc1e67d62b2052346
esraunal <unaal.esra@gmail.com>
2021-12-31 01:23:50 +0300

    ui fix

--- PortfoyYonetimUygulamasi.Host/Controllers/User/Home.cs
+++ PortfoyYonetimUygulamasi.Host/Controllers/User/Home.cs
@@ -11,6 +11,8 @@ using PortfoyYonetimUygulamasi.Entity.User;
 using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
 using RouteAttribute = System.Web.Http.RouteAttribute;
 using RoutePrefixAttribute = System.Web.Http.RoutePrefixAttribute;
+using System.Net;
+
 namespace PortfoyYonetimUygulamasi.Host.Controllers.User
 {
     [RoutePrefix("/api/PortfoyYonetimSitesi")]
@@ -22,27 +24,27 @@ namespace PortfoyYonetimUygulamasi.Host.Controllers.User
         {
             var client = new RestClient("https://api.binance.com/api/v3/ticker/24hr");
             var request = new RestRequest(Method.GET);
-
-            IRestResponse response = client.Execute(request);
-            JsonSerializerSettings settings = new JsonSerializerSettings();
-            settings.NullValueHandling = NullValueHandling.Ignore;
-
-            var myJson = JsonConvert.DeserializeObject(response.Content, settings);
-            JArray jsonArray = JArray.Parse(response.Content);
-
-            List<string> childTokens = new List<string>();
-
-            foreach (var childToken in jsonArray.Children())
-            {
-                childTokens.Add(childToken.ToString());
-            }
-
             List<_24HourExchange> _24HourExchangeList = new List<_24HourExchange>();
 
-            foreach (var x in childTokens)
-            {
-                _24HourExchangeList.Add(JsonConvert.DeserializeObject<_24HourExchange>(x));
-            }
+              IRestResponse response = client.Execute(request);
+              JsonSerializerSettings settings = new JsonSerializerSettings();
+              settings.NullValueHandling = NullValueHandling.Ignore;
+
+              var myJson = JsonConvert.DeserializeObject(response.Content, settings);
+              JArray jsonArray = JArray.Parse(response.Content);
+
+              List<string> childTokens = new List<string>();
+
+              foreach (var childToken in jsonArray.Children())
+              {
+                  childTokens.Add(childToken.ToString());
+              }
+
+
+              foreach (var x in childTokens)
+              {
+                  _24HourExchangeList.Add(JsonConvert.DeserializeObject<_24HourExchange>(x));
+              }
 
             return _24HourExchangeList;
         }
